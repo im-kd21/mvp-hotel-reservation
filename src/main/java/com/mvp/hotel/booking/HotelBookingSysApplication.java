79330b59466f320a9e601615a47cc9e7c2175cb5
Kamal Kant Dwivedi <kamalkantdwivedi@Kamals-MacBook-Air.local>
2024-02-03 15:29:51 +0530

    added azure plugin

--- src/main/java/com/mvp/hotel/booking/HotelBookingSysApplication.java
+++ src/main/java/com/mvp/hotel/booking/HotelBookingSysApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HotelBookingSysApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HotelBookingSysApplication.class, args);
 	}
